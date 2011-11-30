a00b43a2c2589345d66688f26cbcefed47c5f237
ZarraN <saariniemi@gmail.com>
2011-11-30 10:22:46 +0100

    alla functions mot java

---
+++ login.php
@@ -0,0 +1,17 @@
+<?php
+
+class Login(){
+
+	private function __construct(){
+		
+	}
+
+	private function login($userEmail, $userPassword){
+		
+	}
+
+	private function logout($userId){
+		
+	}
+
+}

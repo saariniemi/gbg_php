7e421a19f49d8aa56d28b2f5ed8f43e312d4b2e6
ZarraN <saariniemi@gmail.com>
2011-12-01 10:49:36 +0100

    Stadat upp login filen

--- inc/login.php
+++ inc/login.php
@@ -5,23 +5,17 @@ class Login
 
 	function __construct()
 	{
-		$helloWorld = 'hello world';
+
 	}
 
 	function login($userEmail, $userPassword)
 	{
+		
 	}
 
 	function logout($userId)
 	{
-	}
-	
-	function helloWorld($helloWorld)
-	{
-		 
-		//return $helloWorld;
-		print json_encode($helloWorld);
-		
+
 	}
 
 }
